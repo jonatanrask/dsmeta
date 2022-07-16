f51bb7e26777e69b18e4cbed41a167bf76941ead
Jonatan Rask <jonatan16.veloz@gmail.com>
2022-07-16 09:23:35 -0400

    notificação

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg'
 import { BASE_URL } from '../../utils/request';
+
 import './styles.css'
 
 type Props = {
@@ -11,7 +12,7 @@ function heandleClick(id :number) {
     axios(`${BASE_URL}/sales/${id}/notification`)
         .then(response => {
             toast.info("SMS enviado com sucesso");
-        })
+        });
 }
 
 
