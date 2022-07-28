e3d987df60eb2a9d15857c61faeb9db5090e9a8c
Jonatan Rask <jonatan16.veloz@gmail.com>
2022-07-28 11:40:51 -0400

    Espaçamento do SMS corrigido!

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -34,8 +34,8 @@ public class SmsService {
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
-		String msg = "O Vendedor " + sale.getSellerName() + "Foi destaque em " + date
-				+ "Com um total de R$ " + String.format("%.2f", sale.getAmount());
+		String msg = "O(a) vendedor(a) " + sale.getSellerName() + " foi destaque em " + date
+				+ " com um total de R$ " + String.format("%.2f", sale.getAmount());
 
 		Twilio.init(twilioSid, twilioKey);
 
